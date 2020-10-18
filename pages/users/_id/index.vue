a6ae8ec79118dadf4a97f7ddc6604de00e871a4c
CyVision13 <CyVision13@outlook.com>
2020-10-18 15:01:14 -0400

    Validating Parameters

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -1,3 +1,12 @@
 <template>
     <h1>A single User, with ID: {{ $route.params.id}}</h1>
 </template>
+
+<script>
+export default {
+    validate (data){
+        console.log(data)
+        return /^\d+$/.test(data.params.id)
+    }
+}
+</script>
