e8fe8bed5691101d31c6c537e4c4a9f1fe82dcb7
CyVision13 <CyVision13@outlook.com>
2020-10-19 03:52:07 -0400

    Creating Nested Routes

---
+++ pages/users.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <input v-model="userId" type="text">
+        <button @click="onLoadUser">Load User</button>
+        <nuxt-child />
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            userId: ''
+        }
+    },
+    methods: {
+        onLoadUser(){
+            this.$router.push(`/users/${this.userId}`)
+        }
+    }
+}
+</script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,21 +1,9 @@
 <template>
-    <div>
-        <input v-model="userId" type="text">
-        <button @click="onLoadUser">Load User</button>
-    </div>
+    <h1>Please enter an id and click the button</h1>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            userId: ''
-        }
-    },
-    methods: {
-        onLoadUser(){
-            this.$router.push(`/users/${this.userId}`)
-        }
-    }
+    
 }
 </script>
